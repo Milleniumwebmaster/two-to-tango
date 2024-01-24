7cf5a481aefee33503043e009b22d7adce8aaf3e
milleniumwebmaster <kontakt@millenium.dk>
2024-01-24 10:31:59 +0100

    Update string-with-quotes.js
    I have updated the string-with-quotes.js file. It is now possible to toggle when you click on the id attribute named 'note'

--- js/string-with-quotes.js
+++ js/string-with-quotes.js
@@ -4,18 +4,46 @@ let message;
 
 // Assign values to these variables.
 title = "Sallings's Special Offers";
-message = '<a href=\"sale.html\">50% off!</a>';
+message = '50% off!';
 
 // Get the element with an id of title.
-var elTitle = document.getElementById('title');
+const elTitle = document.getElementById('title');
 // Replace the content of this element.
 elTitle.textContent = title;
 
 // Get the element with an id of note.
-var elNote = document.getElementById('note');
+const elNote = document.getElementById('note');
 // Replace the content of this element.
 elNote.innerHTML = message;
 
+const contentContainer = document.getElementById("content");
+contentContainer.style.backgroundColor = "orange";
+contentContainer.style.width = "250px";
+
+// document.getElementById('note').onclick = function(e) {
+//     alert('Element clicked!');
+//   };
+
+// document.getElementById('note').onclick = function(e) {
+//     if(contentContainer.style.backgroundColor === "orange") {
+//         contentContainer.style.backgroundColor = "lightblue";
+//     } else {
+//         contentContainer.style.backgroundColor = "orange";
+//     } 
+// };
+
+document.getElementById('note').onclick = function() {
+    if(contentContainer.style.backgroundColor === "orange") {
+        contentContainer.style.backgroundColor = "lightblue";
+        elTitle.textContent = "Thank You joining!"
+    } else {
+        contentContainer.style.backgroundColor = "orange";
+        elTitle.textContent = "Sallings's Special Offers"
+    } 
+};
+
+
+  
 /* 
 NOTE: textContent does not work in IE8 or earlier
 You can use innerHTML on line 12, but note the security issues on p228-231
